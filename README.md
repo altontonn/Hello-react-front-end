fa05308d1ea655be25a7177235f0015cf3305a00
Newton <altontonnalumasa@gmail.com>
2023-05-05 14:11:21 +0300

    Remove commented comments

--- README.md
+++ README.md
@@ -111,8 +111,6 @@ Feel free to check the [issues page](https://github.com/altontonn/Hello-react-fr
 <!-- SUPPORT -->
 
 ## ⭐️ Show your support <a name="support"></a>
-
-
 If you like this project give it a ⭐️
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
--- src/redux/greeting.js
+++ src/redux/greeting.js
@@ -1,15 +1,3 @@
-// const url = 'http://127.0.0.1:3000/api/v1/messages';
-// export const fetchMessages = () => async (dispatch) => {
-//   try {
-//     const response = await fetch(url);
-//     const json = await response.data;
-//     console.log(json);
-//     dispatch({ type: 'FETCH_MESSAGES', payload: json });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
